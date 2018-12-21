995b2f03f03f358237b1be1ee8e9007c24ce22cf
github username <crishanks12@gmail.com>
2018-12-20 22:49:08 -0700

    forms and input

--- src/containers/series/index.js
+++ src/containers/series/index.js
@@ -5,16 +5,20 @@ class Series extends Component {
     state = {
         series: []
       }
-    
-      componentDidMount() {
-        fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-          .then(response => response.json())
-          .then(json => this.setState({ series: json }))
-      }
+
+    onSeriesInputChange = a => {
+        fetch(`http://api.tvmaze.com/search/shows?q=${a.target.value}`)
+            .then(response => response.json())
+            .then(json => this.setState({ series: json }))
+    }
+
     render() {
         return (
            <div>
                The length of series array - {this.state.series.length}
+               <div>
+                   <input type='text' onChange={this.onSeriesInputChange} />
+               </div>
                 <SeriesList list={this.state.series} />
            </div>
         )
