9b02423bd3f8999cba9c9d6eb2e096335bafa17d
github username <crishanks12@gmail.com>
2018-12-21 01:15:23 -0700

    using route parameters

--- src/containers/single-series/index.js
+++ src/containers/single-series/index.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import Loader from '../../components/loader';
 
 class SingleSeries extends Component {
+    state = {
+        show: null
+    }
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+
+        fetch(`http://api.tvmaze.com/search/shows/${id}?embed=episodes`)
+            .then(response => response.json())
+            .then(json => this.setState({ show: json}));
+    }
+
     render() {
+        const { show } = this.state;
+        console.log(show);
+
         return (
             <div>
-                <p>Single Series - The show ID will be {this.props.match.params.id}</p>
+                { show === null && <Loader /> }
+                {
+                    show !== null
+                    &&
+                    <div>
+                        <p>{show.name}</p>
+                        <p>Premiered - {show.premiered}</p>
+                        <p>Rating - {show.rating.average}</p>
+                        <p>Episodes - {show._embedded.eposodes.length}</p>
+                        <p>
+                            <img alt="Show" src={show.image.medium} /> 
+                        </p>
+                    </div>
+                }
             </div>
-        );
+        )
     }
 }
 
