073a8cd73c33bf7220f0a207915e5e4f1c2f0152
github username <crishanks12@gmail.com>
2018-12-20 22:08:01 -0700

    added react containers

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
 import Intro from '../intro'
-import logo from './logo.svg';
+import Series from '../../containers/series'
 import './App.css';
 import 'whatwg-fetch';
 
 class App extends Component {
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-      .then(response => resoponse.json())
-      .then(json => consolelog(json))
-  }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
         </header>
         <Intro message="Here you can find all of your most loved series" />
-        The length of series array - {this.state.series.length}
+        <Series />
       </div>
     );
   }
