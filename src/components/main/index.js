be10968d2957f315130a194b7770d21a8d1f2043
github username <crishanks12@gmail.com>
2018-12-21 00:22:22 -0700

    redirecting routes

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Series from '../../containers/series';
+import SingleSeries from '../../containers/single-series';
 
 const Main = props => (
     <Switch>
         <Route exact path="/" component={Series} />
+        <Route path="/series/:id" component={SingleSeries} />  
     </Switch>
 )
 
